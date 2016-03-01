35f1ae73abb6d7764345a23c642b3b2b693ae79d
Ross Phillips <ross.phillips@live.co.uk>
2016-03-01 18:04:27 +0000

    initial code.
    loading from twitter data done.
    to do: loading from crime data.

---
+++ Crime.py
@@ -0,0 +1,14 @@
+class Crime:
+    self.Crime ID
+    self.Month
+    Reported by
+    Falls within
+    Longitude
+    Latitude
+    Location
+    LSOA code
+    LSOA name
+    Crime type
+    Last outcome
+    category
+    Context
