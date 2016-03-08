beca089c770ca2f8e3d4a5db4a3ae5be8d0c4da6
David Twomey <david.twomey@live.com>
2016-03-08 17:55:33 +0000

    Added timeseries processing and started grid bagging DT

--- Tweet.py
+++ Tweet.py
@@ -4,6 +4,7 @@ class Tweet:
     def __init__(self, country, date, latitude, longitude, placeName, placeType, profileLocation, text, tweetId,
                  username):
         self.country = country
+        self.timestamp = datetime.datetime.fromtimestamp(int(date / 1000))
         self.date = datetime.datetime.fromtimestamp(int(date / 1000)).strftime('%d-%m-%Y')
         self.latitude = latitude
         self.longitude = longitude
